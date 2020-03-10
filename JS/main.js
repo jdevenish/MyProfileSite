35def07d51c236307d1dfd6dc04461c300b3a428
jdevenish <justin.devenish@gmail.com>
2020-03-10 16:04:43 -0400

    responsive framework

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,36 @@
 if (typeof $ == 'undefined'){
     console.log('oops! I still have to link my jQuery properly!');
 } else {console.log('I did it! I linked jQuery and this js file properly!')};
+
+
+$("#education").click(function() {
+    if($( window ).width() < 768){
+        $( "#eduContent" ).toggleClass("hideContent" );
+    } else{
+        // $( "#eduContent" ).css("display", "none" );
+    }
+});
+
+$( "#workHistory" ).click(function() {
+    if($( window ).width() < 768){
+        $( "#workContent" ).toggleClass("hideContent" );
+    }
+});
+
+$( "#certifications" ).click(function() {
+    if($( window ).width() < 768){
+        $( "#certContent" ).toggleClass("hideContent" );
+    }
+});
+
+$( "#awards" ).click(function() {
+    if($( window ).width() < 768){
+        $( "#awardsContent" ).toggleClass("hideContent" );
+    }
+});
+
+$( "#patents" ).click(function() {
+    if($( window ).width() < 768){
+        $( "#patentsContent" ).toggleClass("hideContent" );
+    }
+});
